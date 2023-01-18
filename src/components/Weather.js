f4e3033a6e5824705a7712d4b32d5b221fca8366
Brennan Malone <malone.brennan98@gmail.com>
2023-01-18 17:07:18 -0500

    completed render of Weather.js

---
+++ src/components/Weather.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { ListGroup } from 'react-bootstrap';
+import './Weather.css';
+
+class Weather extends React.Component {
+  render() {
+
+    return (
+      <>
+        {this.props.weatherData.map((day, index) => {
+          return <ListGroup.Item key={index}>{day.date}, {day.description}</ListGroup.Item>
+        })
+        };
+      </>
+    )
+  }
+}
+
+export default Weather;
